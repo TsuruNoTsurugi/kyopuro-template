4a45156d37419d2eb6f9e8024e4eb11e2114639c
TsuruNoTsurugi <104486593+TsuruNoTsurugi@users.noreply.github.com>
2024-03-12 22:40:21 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// author: TsuruNoTsurugi
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define precision std::fixed<<std::setprecision(15)
+#define pow_ll(a, b) static_cast<ll>(pow(a, b))
+
+int main(){
+
+}
